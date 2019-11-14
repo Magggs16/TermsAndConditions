5e4ceb1b9b40ea42fa7cd67723bc25b145ad9b07
Magggs16 <Maggieamador16@gmail.com>
2019-11-14 17:37:09 -0600

    Update T&Cs.py
    Grab each page and parse into sentences

--- T&Cs.py
+++ T&Cs.py
@@ -1,3 +1,4 @@
+
 import PyPDF2
 
 bTos = open("TOS.pdf","rb") #rb for read binary mode
@@ -7,15 +8,38 @@ print(pdfTos.numPages)
 
 #get the page and extract text
 print("Text by sentence :\n")
-pageObj = pdfTos.getPage(0)
+pageObj = pdfTos.getPage(1)
 
-page = pageObj.extractText().replace('\n',' ')
+page = pageObj.extractText().replace('\n',' ').replace('  ',' ')
+
 pageSentences = ""
+
+
+#****TO DO
+#put all letters in an array and check if two spaces
+#follow eachother replace one space as an '\n'
+#then put each sentence in an array
+sentenceCnt = 0
 for t in page:
     if t == '.' :
-        t = t.replace(".",".\n")
+        t = t.replace(".",".\n\n")
+        sentenceCnt+=1
     pageSentences+=t
 print(pageSentences)
+
+#make page sentences array
+sentences = ["" for x in range(sentenceCnt)]
+print(sentenceCnt)
+pageSentences = ""
+for i in range(0,sentenceCnt):
+    print(i)
+    for t in page:
+        if t == '.' :
+            sentences[i] = pageSentences
+            pageSentences = ""
+        pageSentences+=t
+
+
      
 # closing the pdf file object 
 bTos.close() 
